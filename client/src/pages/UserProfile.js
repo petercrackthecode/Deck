a929900ced6f6b39af984f9a37c9afef04a3d5b9
anjali-001 <anjali001.as@gmail.com>
2021-04-05 00:45:31 +0530

    anjali made a stupid error, resolved now

--- client/src/pages/UserProfile.js
+++ client/src/pages/UserProfile.js
@@ -14,18 +14,19 @@ function UserProfile({userId}) {
 
     const handleDetails = async() => {
         console.log(userId)
-        await axios.get(`http://localhost:3000/users/${userId}`, {
+        await axios.get(`http://localhost:5000/users/${userId}`, {
             headers:{"x-access-token":getToken()}
         })
         .then((res)=> {
-            setUser(res.user)
+            setUser(res.data)
+            console.log(res.data.name)
         })
         .catch((err)=> err)
     }
 
     return (
         <div>
-            <h3>Name</h3>
+            <h3>{user.name}</h3>
             <p>Company Name</p>
             <button>Edit Profile</button>
         </div>
