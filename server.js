e4f69637ce1afd71ee70f87ce38a55500658a4df
petercrackthecode <nguyenvandung02051999@gmail.com>
2021-04-04 17:21:18 -0700

    Add file to deploy to heroku

--- server.js
+++ server.js
@@ -1,26 +1,28 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const dotenv = require('dotenv');
-const cors = require('cors');
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const cors = require("cors");
 //AuthRoutes
-var UserController = require('./user/UserController'); 
-var AuthController = require('./auth/AuthController');
+var UserController = require("./user/UserController");
+var AuthController = require("./auth/AuthController");
 
 const app = express();
 app.use(cors());
 
 //connect to DB
 dotenv.config();
-mongoose.connect(process.env.DB_CONNECT,{useNewUrlParser:true,useUnifiedTopology:true})
-.then(()=>console.log("Connected to MongoDB"))
-.catch((err)=> console.log(err))
+mongoose
+  .connect(process.env.DB_CONNECT, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((err) => console.log(err));
 //Middleware
 app.use(express.json());
-app.use(express.urlencoded({extended:false}))
+app.use(express.urlencoded({ extended: false }));
 
-app.use('/api/auth', AuthController);
-app.use('/users', UserController);
+app.use("/api/auth", AuthController);
+app.use("/users", UserController);
 
-
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, ()=>console.log('Server up and running'));
+app.listen(process.env.PORT, () => console.log("Server up and running"));
