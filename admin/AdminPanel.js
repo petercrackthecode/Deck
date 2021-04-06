f2d64816c4cd27fed6b223d1f09295c0089fd37e
anjali-001 <anjali001.as@gmail.com>
2021-04-06 18:18:19 +0530

    admin access backend

--- admin/AdminPanel.js
+++ admin/AdminPanel.js
@@ -6,23 +6,40 @@ var bcrypt = require('bcryptjs');
 router.use(bodyParser.urlencoded({ extended: false }));
 router.use(bodyParser.json());
 var User = require('../user/User');
+const { response } = require('express');
 
+const isAdmin = (req,res,next) => {
+var userId = req.body._id;
+User.findOne({_id:userId}, (err,doc)=>{
+  if(doc.admin!==true)
+  {
+    res.send('Access Denied')
+  }
+  else{
+    next();
+  }
+  
+})
+}
 
-router.get('/search-user', (req,res) => {
+router.post('/search-user',isAdmin, (req,res) => {
   var email = req.body.email;
   User.findOne({email:email},(err,doc)=>{
     if(doc)
     {
       res.send(doc.domains)
     }
+    else{
+      res.send("The Email doesn't exist in the decks database")
+    }
   })
 })
 
-router.post('/status',(req,res)=>{
+router.post('/status',isAdmin, (req,res)=>{
   var updatedStatus=req.body.status;
   User.findOneAndUpdate({email:req.body.email},{domains:updatedStatus},{new:true})
-  .then(res=>res.send('Data updated successfully'))
+  .then(res=>console.log('Data updated successfully',res))
   .catch(err=>console.log(err))
 })
 
-  module.exports = router;
+module.exports = router;
