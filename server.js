b595e89599ac2e1aaf04ee4cb10540948f912434
petercrackthecode <nguyenvandung02051999@gmail.com>
2021-04-06 17:46:54 -0700

    Successfully pass back the access token to Autobot to log users in

--- server.js
+++ server.js
@@ -25,4 +25,35 @@ app.use(express.urlencoded({ extended: false }));
 app.use("/api/auth", AuthController);
 app.use("/users", UserController);
 
-app.listen(process.env.PORT, () => console.log("Server up and running"));
+let accessToken = null,
+    user = null;
+
+app.get("/api/get-access-token", (req, res) => { // auto bot will communicate with this route only.
+  const checkAccessToken = setInterval(() => {
+    if (accessToken && user) {
+      console.log(`Hi I'm the access token = ${accessToken}`);
+      clearInterval(checkAccessToken);
+      res.send({"accessToken": accessToken, "user": user});
+      // clean up the accessToken and user on the server memory after passing them out.
+    }
+    else console.log("I'm not activated");
+
+    accessToken = null;
+    user = null;
+  }, 500);
+});
+
+app.post("/api/test", (req, res, next) => {
+  res.status(200).send("Testing");
+});
+
+app.post("/api/pass-access-token", (req, res, next) => {
+  console.log(`Hi this is req.body`);
+  console.log(req.body);
+  accessToken = req.body.accessToken;
+  user = req.body.user;
+  res.status(200).send("Access token is successfully passed");
+  next();
+});
+
+app.listen(process.env.PORT, () => console.log(`Server up and running at ${process.env.PORT}`));
