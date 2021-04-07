35da8f0eca32b2e002715d679eebb41ede6cf1f1
petercrackthecode <nguyenvandung02051999@gmail.com>
2021-04-06 18:59:06 -0700

    Successfully resolve a merge conflict. Add the new client login with the Deck API

--- auth/AuthController.js
+++ auth/AuthController.js
@@ -37,70 +37,86 @@ router.post("/register", (req, res) => {
         });
         const salt = await bcrypt.genSalt(10);
         const hashedPassword = await bcrypt.hash(req.body.password, salt);
-    User.findOne({email:req.body.email}, async(error,doc)=>{
-      if(doc)
-      return res.send({error:'Email already exists! Sign in instead.',user:null,token:null});
-      const domList=[]
-      Domain.findOne({company_name:req.body.company_name},async (err,doc)=>{
-        doc.domain_list.forEach((item)=>{
-          var temp={
-            name:item,
-            status:"pending"
-          }
-          domList.push(temp)
-        })
-        const salt = await bcrypt.genSalt(10)
-        const hashedPassword = await bcrypt.hash(req.body.password,salt);
-        const user = new User({
-          name: req.body.name,
-          email: req.body.email,
-          password: hashedPassword,
-          token: jwt.sign({ email: req.body.email }, process.env.TOKEN_SECRET),
-          company_name: req.body.company_name,
-          domains: domList,
+        User.findOne({ email: req.body.email }, async (error, doc) => {
+          if (doc)
+            return res.send({
+              error: "Email already exists! Sign in instead.",
+              user: null,
+              token: null,
+            });
+          const domList = [];
+          Domain.findOne(
+            { company_name: req.body.company_name },
+            async (err, doc) => {
+              doc.domain_list.forEach((item) => {
+                var temp = {
+                  name: item,
+                  status: "pending",
+                };
+                domList.push(temp);
+              });
+              const salt = await bcrypt.genSalt(10);
+              const hashedPassword = await bcrypt.hash(req.body.password, salt);
+              const user = new User({
+                name: req.body.name,
+                email: req.body.email,
+                password: hashedPassword,
+                token: jwt.sign(
+                  { email: req.body.email },
+                  process.env.TOKEN_SECRET
+                ),
+                company_name: req.body.company_name,
+                domains: domList,
+              });
+              user
+                .save()
+                .then((resp) => {
+                  res.send({ user: resp, error: null });
+                })
+                .catch((err) => res.status(400).send(err));
+            }
+          );
+
+          //Hash Passwords
         });
-        user
-          .save()
-          .then((resp) => {
-            res.send({ user: resp, error: null });
-          })
-          .catch((err) => res.status(400).send(err));
       }
     );
-
-    //Hash Passwords
   });
-});
 
-router.get("/me", VerifyToken, (req, res) => {
-  var token = req.headers["x-access-token"];
-  if (!token)
-    return res.status(401).send({ auth: false, message: "No token provided." });
-
-  jwt.verify(token, process.env.TOKEN_SECRET, (err, decoded) => {
-    if (err)
+  router.get("/me", VerifyToken, (req, res) => {
+    var token = req.headers["x-access-token"];
+    if (!token)
       return res
-        .status(500)
-        .send({ auth: false, message: "Failed to authenticate token." });
-    console.log(decoded);
-    User.findById(
-      decoded._id,
-      { password: 0 }, // projection
-      (err, user) => {
-        if (err)
-          return res.status(500).send("There was a problem finding the user.");
-        if (!user) return res.status(404).send("No user found.");
+        .status(401)
+        .send({ auth: false, message: "No token provided." });
 
-        res.status(200).send(user);
-      }
-    );
-      //token 
-      const token = jwt.sign({_id: doc._id}, process.env.TOKEN_SECRET, {
-        expiresIn:86400
-      })
-      res.header('auth-token',token).send({error:null, auth:true,user:doc,token:token})//user data error null
-  })
-    
+    jwt.verify(token, process.env.TOKEN_SECRET, (err, decoded) => {
+      if (err)
+        return res
+          .status(500)
+          .send({ auth: false, message: "Failed to authenticate token." });
+      console.log(decoded);
+      User.findById(
+        decoded._id,
+        { password: 0 }, // projection
+        (err, user) => {
+          if (err)
+            return res
+              .status(500)
+              .send("There was a problem finding the user.");
+          if (!user) return res.status(404).send("No user found.");
+
+          res.status(200).send(user);
+        }
+      );
+      //token
+      const token = jwt.sign({ _id: doc._id }, process.env.TOKEN_SECRET, {
+        expiresIn: 86400,
+      });
+      res
+        .header("auth-token", token)
+        .send({ error: null, auth: true, user: doc, token: token }); //user data error null
+    });
   });
 });
 
@@ -133,19 +149,21 @@ router.post("/login", (req, res) => {
       .send({ error: null, auth: true, user: doc, token: token }); //user data error null
   });
 });
-  router.post('/update', VerifyToken, async(req,res)=> {
-    const salt = await bcrypt.genSalt(10);
-    if(req.body?.password)
-    {
-      var hashedPassword = await bcrypt.hash(req.body.password,salt);
-    }
-    var presentData = await User.findOne({_id:req.body._id})
-    presentData.name=req.body?.name
-    presentData.company_name=req.body?.company_name
-    req.body?.password? presentData.password=hashedPassword : presentData=presentData;
-    var update = await User.findOneAndUpdate({_id:req.body._id}, presentData,{new:true})
-    res.json(update) 
-  })
-
+router.post("/update", VerifyToken, async (req, res) => {
+  const salt = await bcrypt.genSalt(10);
+  if (req.body?.password) {
+    var hashedPassword = await bcrypt.hash(req.body.password, salt);
+  }
+  var presentData = await User.findOne({ _id: req.body._id });
+  presentData.name = req.body?.name;
+  presentData.company_name = req.body?.company_name;
+  req.body?.password
+    ? (presentData.password = hashedPassword)
+    : (presentData = presentData);
+  var update = await User.findOneAndUpdate({ _id: req.body._id }, presentData, {
+    new: true,
+  });
+  res.json(update);
+});
 
 module.exports = router;
